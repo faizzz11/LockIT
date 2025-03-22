dd74de2aea3f79773086b1134dc058c83c95f13a
faizzz11 <faizmoulavi54@gmail.com>
2025-03-22 22:18:53 +0530

    fixed some minor issues

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="w-full min-h-[80vh] [background:radial-gradient(125%_125%_at_50%_10%,#1a1a1a_40%,#63e_100%)]">
-        <div className="container mx-auto mb-20">
+      <div className="w-full min-h-[90vh] [background:radial-gradient(125%_125%_at_50%_10%,#1a1a1a_40%,#63e_100%)]">
+        <div className="container mx-auto mb-21">
           <Manager />
         </div>
         <Footer />
--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -28,16 +28,27 @@ const Manager = () => {
   };
 
   const savePass = () => {
+
+    if(Form.site.length >4 && Form.username.length >3 &&Form.pass.length >4){
     const newEntry = { ...Form, id: uuidv4() };
-
     const updatedPassArr = [...PassArr, newEntry];
-
     setPassArr(updatedPassArr);
     localStorage.setItem("passwords", JSON.stringify(updatedPassArr));
-
     console.log(updatedPassArr);
-
     setForm({ site: "", username: "", pass: "" });
+    toast("Details Saved!!", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  } else{
+    toast('Error: Insufficient Characters');
+  }
   };
 
   const DltPass = (id) => {
@@ -100,69 +111,69 @@ const Manager = () => {
         pauseOnHover
         theme="dark"
       />
-    <div className="px-5 pt-14">
-      <div className="manager bg-slate-100 mycontainer rounded-md max-w-6xl mx-auto">
-        <h1 className="text-center font-bold tracking-wide text-[#840cca] text-4xl">
+    <div className="px-5 pt-14 mb-25">
+      <div className="manager bg-purple-50 mycontainer rounded-md max-w-6xl mx-auto p-4 md:p-6 lg:p-8">
+        <h1 className="text-center font-bold tracking-wide text-[#840cca] text-2xl sm:text-3xl md:text-4xl">
           Lock
           <span className="text-[#16052a] text-[gradient-to-br from-[#840cca] to-[#4514a0]]">
             IT
           </span>
         </h1>
-        <p className="text-[#840cca] text-lg text-center">
+        <p className="text-[#840cca] text-base sm:text-lg text-center font-[McLaren]">
           Your Own Password Diary
         </p>
 
-        <div className="text-black flex flex-col p-4 gap-7 items-center">
+        <div className="text-black flex flex-col p-2 sm:p-4 gap-4 sm:gap-7 items-center">
           <input
             value={Form.site}
             onChange={handleChange}
-            className="border border-[#840cca]  rounded-full w-full p-4 py-1"
+            className="border border-[#840cca] rounded-full w-full p-2 sm:p-4 py-1"
             placeholder="Enter Website URL"
             type="text"
             name="site"
             id=""
           />
 
-          <div className="flex w-full justify-between gap-7">
-            <input
-              value={Form.username}
-              onChange={handleChange}
-              className="border border-[#840cca] rounded-full w-full p-4 py-1"
-              placeholder="Enter Username"
-              type="text"
-              name="username"
-              id=""
-            />
+<div className="flex flex-col sm:flex-row w-full justify-between gap-4 sm:gap-7">
+  <input
+    value={Form.username}
+    onChange={handleChange}
+    className="border border-[#840cca] rounded-full w-full sm:flex-[2] p-2 sm:p-4 py-1"
+    placeholder="Enter Username"
+    type="text"
+    name="username"
+    id=""
+  />
 
-            <div className="relative">
-              <input
-                ref={passRef}
-                value={Form.pass}
-                onChange={handleChange}
-                className="border border-[#840cca] rounded-full w-full p-4 py-1"
-                placeholder="Enter Password"
-                type="password"
-                name="pass"
-                id=""
-              />
-              <span
-                className="absolute right-[3px] top-[3px] cursor-pointer"
-                onClick={showPassword}
-              >
-                <img
-                  ref={ref}
-                  className="p-1"
-                  width={28}
-                  src="/icons/eye.png"
-                  alt="eye"
-                />
-              </span>
-            </div>
-          </div>
+  <div className="relative w-full sm:flex-[1]">
+    <input
+      ref={passRef}
+      value={Form.pass}
+      onChange={handleChange}
+      className="border border-[#840cca] rounded-full w-full p-2 sm:p-4 py-1"
+      placeholder="Enter Password"
+      type="password"
+      name="pass"
+      id=""
+    />
+    <span
+      className="absolute right-[3px] top-1/2 transform -translate-y-1/2 cursor-pointer"
+      onClick={showPassword}
+    >
+      <img
+        ref={ref}
+        className="p-1"
+        width={46}
+        src="/icons/eye.png"
+        alt="eye"
+      />
+    </span>
+  </div>
+</div>
 
           <button
             onClick={savePass}
-            className="flex hover:text-white justify-center items-center hover:bg-purple-600 gap-2 px-8 py-2 w-fit rounded-full bg-[#840cca] cursor-pointer border-[2px] border-purple-900"
+            className="flex hover:text-white justify-center items-center hover:bg-purple-600 gap-2 px-6 sm:px-8 py-1 sm:py-2 w-fit rounded-full bg-[#840cca] cursor-pointer border-[2px] border-purple-900"
           >
             <lord-icon
               src="https://cdn.lordicon.com/jgnvfzqg.json"
@@ -171,31 +182,31 @@ const Manager = () => {
             Save
           </button>
         </div>
-        <div className="passwords">
-          <h2 className="font-bold text-2xl py-3">Your Saved Passwords</h2>
+        <div className="passwords overflow-x-auto">
+          <h2 className="font-bold font-[McLaren] text-xl sm:text-2xl py-3">Your Saved Passwords</h2>
           {PassArr.length === 0 && <div>Add Some Password To View</div>}
 
           {PassArr.length != 0 && (
-            <table className="table-auto w-full overflow-hidden rounded-lg">
+            <table className="table-auto w-full overflow-hidden rounded-lg text-xs sm:text-sm md:text-base">
               <thead className="bg-[#840cca] text-white">
                 <tr>
-                  <th className="py-2">WebSite</th>
-                  <th className="py-2">UserName</th>
-                  <th className="py-2">Password</th>
-                  <th className="py-2">Actions</th>
+                  <th className="py-1 sm:py-2 px-1 sm:px-2">WebSite</th>
+                  <th className="py-1 sm:py-2 px-1 sm:px-2">UserName</th>
+                  <th className="py-1 sm:py-2 px-1 sm:px-2">Password</th>
+                  <th className="py-1 sm:py-2 px-1 sm:px-2">Actions</th>
                 </tr>
               </thead>
               <tbody className="bg-purple-200">
                 {PassArr.map((item, index) => {
                   return (
                     <tr key={index}>
-                      <td className="py-2 border border-white text-center">
+                      <td className="py-1 sm:py-2 border border-white text-center px-1 sm:px-2 truncate max-w-[100px] sm:max-w-none">
                         <div className="flex items-center justify-center">
-                          <a href={item.site} target="_blank">
+                          <a href={item.site} target="_blank" className="truncate">
                             {item.site}
                           </a>
                           <div
-                            className="lordiconcopy size-7 cursor-pointer"
+                            className="lordiconcopy size-7 cursor-pointer hidden md:block"
                             onClick={() => {
                               copyText(item.site);
                             }}
@@ -213,11 +224,11 @@ const Manager = () => {
                           </div>
                         </div>
                       </td>
-                      <td className="py-2 border border-white text-center">
+                      <td className="py-1 sm:py-2 border border-white text-center px-1 sm:px-2 truncate max-w-[80px] sm:max-w-none">
                         <div className="flex items-center justify-center">
-                          {item.username}
+                          <span className="truncate">{item.username}</span>
                           <div
-                            className="lordiconcopy size-7 cursor-pointer"
+                            className="lordiconcopy size-7 cursor-pointer hidden md:block"
                             onClick={() => {
                               copyText(item.username);
                             }}
@@ -235,11 +246,11 @@ const Manager = () => {
                           </div>
                         </div>
                       </td>
-                      <td className="py-2 border border-white text-center">
+                      <td className="py-1 sm:py-2 border border-white text-center px-1 sm:px-2 truncate max-w-[80px] sm:max-w-none">
                         <div className="flex items-center justify-center">
-                          <span>{item.pass}</span>
+                          <span className="truncate">{item.pass}</span>
                           <div
-                            className="lordiconcopy size-7 cursor-pointer"
+                            className="lordiconcopy size-7 cursor-pointer hidden md:block"
                             onClick={() => {
                               copyText(item.pass);
                             }}
@@ -258,7 +269,7 @@ const Manager = () => {
                         </div>
                       </td>
 
-                      <td className="justify-center py-2 border border-white text-center">
+                      <td className="justify-center py-1 sm:py-2 border border-white text-center px-1 sm:px-2">
                         <span
                           className="cursor-pointer mx-2"
                           onClick={() => {
@@ -291,6 +302,7 @@ const Manager = () => {
             </table>
           )}
         </div>
+        <div className="h-[100px] block md:hidden"></div>
       </div>
     </div>
     </>
